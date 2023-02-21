def6bac1df2bb70085f53075dd33b02e40a04be5
xiaoyuer <1013462975@qq.com>
2023-02-21 12:55:39 +0800

    your commit message

---
+++ App.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<img src="./assets/logo.png" alt="logo">
+		<MySchool></MySchool>
+		<MyStudent></MyStudent>
+	</div>
+</template>
+
+<script>
+import MySchool from './components/MySchool.vue';
+import MyStudent from './components/MyStudent.vue';
+
+export default {
+	components: {
+		MySchool,
+		MyStudent
+	}
+}
+</script>
